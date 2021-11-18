e959237dc57fc33ea4c5cc97381806142de37726
Ramon Perez <ramonpzg@protonmail.com>
2021-11-19 10:09:28 +1100

    Testing CatBoost

--- src/full_pipe/train.py
+++ src/full_pipe/train.py
@@ -1,6 +1,6 @@
 
 import os, pickle, sys, pandas as pd
-from lightgbm import LGBMRegressor
+from catboost import CatBoostRegressor
 
 input_data = sys.argv[1]
 output = os.path.join('models', 'rf_model.pkl')
@@ -9,7 +9,7 @@ seed, n_est = 42, 100
 X_train = pd.read_csv(input_data)
 y_train = X_train.pop('rented_bike_count')
 
-rf = LGBMRegressor(n_estimators=n_est, random_state=seed)
+rf = CatBoostRegressor(n_estimators=n_est, random_state=seed)
 rf.fit(X_train.values, y_train.values)
 
 with open(output, "wb") as fd: pickle.dump(rf, fd)
