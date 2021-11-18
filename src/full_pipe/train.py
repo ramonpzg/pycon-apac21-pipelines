a54d4faa28eb9b8d1432d162498cf02a5dfb39cb
Ramon Perez <ramonpzg@protonmail.com>
2021-11-18 23:47:09 +1100

    testing ends

--- src/full_pipe/train.py
+++ src/full_pipe/train.py
@@ -1,6 +1,6 @@
 
 import os, pickle, sys, pandas as pd
-from xgboost import XGBRFRegressor
+from lightgbm import LGBMRegressor
 
 input_data = sys.argv[1]
 output = os.path.join('models', 'rf_model.pkl')
@@ -9,7 +9,7 @@ seed, n_est = 42, 100
 X_train = pd.read_csv(input_data)
 y_train = X_train.pop('rented_bike_count')
 
-rf = XGBRFRegressor(n_estimators=n_est, seed=seed)
+rf = LGBMRegressor(n_estimators=n_est, random_state=seed)
 rf.fit(X_train.values, y_train.values)
 
 with open(output, "wb") as fd: pickle.dump(rf, fd)
