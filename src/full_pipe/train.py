abfa62b25aff845babb9c250f7304613dea95e04
Ramon Perez <ramonpzg@protonmail.com>
2021-11-18 23:41:51 +1100

    Testing XGBoost

--- src/full_pipe/train.py
+++ src/full_pipe/train.py
@@ -1,18 +1,15 @@
 
-import os, pickle, sys
-import numpy as np, pandas as pd
-from sklearn.ensemble import RandomForestRegressor
+import os, pickle, sys, pandas as pd
+from xgboost import XGBRFRegressor
 
 input_data = sys.argv[1]
 output = os.path.join('models', 'rf_model.pkl')
-seed = 42
-n_est = 100
+seed, n_est = 42, 100
 
 X_train = pd.read_csv(input_data)
 y_train = X_train.pop('rented_bike_count')
 
-rf = RandomForestRegressor(n_estimators=n_est, random_state=seed)
+rf = XGBRFRegressor(n_estimators=n_est, seed=seed)
 rf.fit(X_train.values, y_train.values)
 
-with open(output, "wb") as fd:
-    pickle.dump(rf, fd)
+with open(output, "wb") as fd: pickle.dump(rf, fd)
