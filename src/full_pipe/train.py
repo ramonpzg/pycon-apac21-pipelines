f3a7085d66863ddb57a734573065b968ebf75155
Ramon Perez <ramonpzg@protonmail.com>
2021-11-19 10:05:23 +1100

    Training 1 completed

--- src/full_pipe/train.py
+++ src/full_pipe/train.py
@@ -1,23 +1,18 @@
 
-import os, pickle, sys, pandas as pd
-<<<<<<< HEAD
-from catboost import CatBoostRegressor
-=======
-from lightgbm import LGBMRegressor
->>>>>>> a1ecba27c1cfae510ef08a9a83ffd42ec7544b0b
+import os, pickle, sys
+import numpy as np, pandas as pd
+from sklearn.ensemble import RandomForestRegressor
 
 input_data = sys.argv[1]
 output = os.path.join('models', 'rf_model.pkl')
-seed, n_est = 42, 100
+seed = 42
+n_est = 100
 
 X_train = pd.read_csv(input_data)
 y_train = X_train.pop('rented_bike_count')
 
-<<<<<<< HEAD
-rf = CatBoostRegressor(n_estimators=n_est, random_state=seed)
-=======
-rf = LGBMRegressor(n_estimators=n_est, random_state=seed)
->>>>>>> a1ecba27c1cfae510ef08a9a83ffd42ec7544b0b
+rf = RandomForestRegressor(n_estimators=n_est, random_state=seed)
 rf.fit(X_train.values, y_train.values)
 
-with open(output, "wb") as fd: pickle.dump(rf, fd)
+with open(output, "wb") as fd:
+    pickle.dump(rf, fd)
